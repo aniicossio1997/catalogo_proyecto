1978ceb1b2e075a1c8e934095df13a1dc9dd87da
CanosaLeandro <canosa.leandro@gmail.com>
2019-11-08 15:24:51 -0300

    Arreglado el menu toggle

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,11 @@
-$("#menu-toggle").click(function(e) {
+$(document).on('turbolinks:load', function () {
+  $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
+});
+
+$(document).on('click', ".js-card-checkbox", function(){
+  //alert("Cantidad de checkboxes :"+$(".js-card-checkbox").length);
+  $(".js-card-checkbox").not(this).prop("checked", false);
+});
