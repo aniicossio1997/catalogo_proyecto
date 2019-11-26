39a642f76cc155aebdf058421c55dee29a338768
CanosaLeandro <canosa.leandro@gmail.com>
2019-11-26 13:28:55 -0300

    Seed completo para crear productos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,27 @@
   Category.create(name: 'Bazar')
   Category.create(name: 'Jugueteria')
   Category.create(name: 'Electronica')
+  Category.create(name: '')
 
   Tag.create(name: 'historico')
   Tag.create(name: 'cosas generales')
 
-  Config.create(per_page: 3)
+  5.times do |i|
+    p = Product.new(name: "Product ##{i}",
+                    description: 'A product.',
+                    price: 278,
+                    cost: 200,
+                    active: true,
+                    code: i,
+                    category_id: 1
+                  )
+    p.product_images.build(principal: true)
+    p.save
+  end
+
+  Buy.create(state: 'pending', user: User.first)
+  Buy.create(state: 'accepted', user: User.first)
+  Buy.create(state: 'rejected', user: User.first)
+
+  Item.create(product: Product.first, price: Product.first.price,count: 3, buy: Buy.first)
+  Item.create(product: Product.last, price: Product.last.price,count: 2, buy: Buy.last)
