56688318e1d414636e76728d384493f0983bb8e3
CanosaLeandro <canosa.leandro@gmail.com>
2019-12-06 15:27:54 -0300

    Corregido conflicto en routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  devise_for :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  unauthenticated do
-    root 'frontend/main#index'
-  end
-
-  authenticated do
-    root 'backend/main#index'
-=======
   devise_for :users, controllers: { registrations: 'users/registrations' }
   scope module: :frontend do
     root to: 'main#home'
@@ -20,7 +9,6 @@ Rails.application.routes.draw do
     get 'cart', to: 'cart#show'
     get 'add_product_to_cart/:id', to: 'cart#add_to_cart', as: 'add_product_to_cart'
     get 'remove_from_cart/:id', to: 'cart#remove_from_cart', as: 'remove_product_from_cart'
->>>>>>> Stashed changes
   end
 
   namespace :backend do
@@ -29,8 +17,6 @@ Rails.application.routes.draw do
     resources :tags
     resources :sliders
     resources :products
-<<<<<<< Updated upstream
-=======
     resources :clients, only: %i[index destroy]
     resources :configs, except: %i[show destroy index edit] do
       get 'edit', on: :collection
@@ -44,7 +30,6 @@ Rails.application.routes.draw do
       # get ':id/:context/state_change_accepted', to: 'buys#state_change_accepted' , as: 'state_change_accepted'
       # get ':id/:context/state_change_rejected', to: 'buys#state_change_rejected' , as: 'state_change_rejected'
     end
->>>>>>> Stashed changes
   end
 
 end
